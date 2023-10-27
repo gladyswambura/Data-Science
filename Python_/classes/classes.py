f31bbfb40c8531a488688dd019f0d03badb681a8
Kipkosgeii <odkhoder@gmail.com>
2023-10-27 14:07:43 +0300

    "Update On Classes"

--- Python_/classes/classes.py
+++ Python_/classes/classes.py
@@ -1,33 +1,49 @@
 # A class in Python is a blueprint or a template for creating objects. It defines the structure and behavior that the objects created from it will have. Objects are instances of classes. To create a class in Python, you use the class keyword followed by the class name. Class names are typically written in CamelCase.
-
+#. objects, classes,encapsulation, inheritance
 # The class keyword is followed by the class name. The class name is the name of the class.
-
 class Person:
+  #  constructor access object attribute
+
+    # attributes
+    gender = 'male'
+
+    # 
     def __init__(self, name, age):
+        # initializing instance variable 
         self.name = name  # Attribute
         self.age = age    # Attribute
 
     def say_hello(self):  # Method
         print(f"Hello, my name is {self.name} and I'm {self.age} years old.")
 
+student1=Person('Adrian',35)
+student2 = Person('Lona', 22)
+student1.say_hello()
+print(student1.name)
+
+
+# accesing attributes inside the class
+print('This is an attribute', Person.gender)
+
+
 
 # classes
 class Email:
-  # __ means private
-  __read = False
+  # __ means private underscore
+    __read = False
+    
+    def __init__(self, subject, body):
+      self.subject = subject
+      self._body = body
 
-  def __init__(self, subject, body):
-    self.subject = subject
-    self.body = body
+    def mark_as_read(self):
+      self.__read = True
 
-  def mark_as_read(self):
-    self.__read = True
+    def is_read(self):
+      return self.__read
 
-  def is_read(self):
-    return self.__read
-
-  def is_spam(self):
-    return "you won 1 million" in self.subject
+    def is_spam(self):
+      return "you won 1 million" in self.subject
 
 e = Email("Check this out", "There are a bunch of free online course here: https://course.online")
 print(e.is_spam()) # False
@@ -43,7 +59,7 @@ class Student(Person):
         self.student_id = student_id  # Adding a new attribute
 
     def study(self):
-        print(f"{self.name} is studying.")
+        print(f"{self.name} is studying.{self.age}, {self.student_id}")
 
 
 # inheritance example 2
@@ -72,4 +88,8 @@ class MyClass:
 obj = MyClass()
 print(obj.get_private_var())  # Accessing the private variable through a method
 # print(obj.__private_var)     # This would raise an AttributeError
+
+# Encapsulation
+# polymorphism
+
 
